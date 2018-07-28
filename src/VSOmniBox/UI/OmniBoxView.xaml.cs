39f58c9bc9481c9a6b761239e9280196b9388fa2
Christian Gunderman <gundermanc@gmail.com>
2018-07-27 19:23:35 -0700

    Delete buggy positioning code.

--- src/VSOmniBox/UI/OmniBoxView.xaml.cs
+++ src/VSOmniBox/UI/OmniBoxView.xaml.cs
@@ -3,7 +3,6 @@
     using System.Diagnostics;
     using System.Windows;
     using System.Windows.Input;
-    using System.Windows.Media;
     using VSOmniBox.API.Data;
 
     /// <summary>
@@ -14,35 +13,6 @@
         public OmniBoxView()
         {
             this.InitializeComponent();
-            this.WindowStartupLocation = WindowStartupLocation.Manual;
-            var docWell = FindVisualChildren(Application.Current.MainWindow, "Microsoft.VisualStudio.PlatformUI.Shell.Controls.DocumentGroupContainerControl");
-            var topLeftAnchor = docWell.PointToScreen(new Point(0, 0));
-            this.Top = topLeftAnchor.Y;
-            this.Left = (topLeftAnchor.X + docWell.ActualWidth) / 2 - this.Width/2;
-        }
-
-        public static FrameworkElement FindVisualChildren(DependencyObject depObj, string typeName) 
-        {
-            if (depObj != null)
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-                {
-                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null)
-                    {
-                        var type = child.GetType();
-                        if (type.FullName.Equals(typeName, System.StringComparison.OrdinalIgnoreCase))
-                        {
-                            return child as FrameworkElement;
-                        }
-                        var containsSpecifiedChild = FindVisualChildren(child, typeName);
-                        if (containsSpecifiedChild != null)
-                            return containsSpecifiedChild as FrameworkElement;
-
-                    }
-                }
-            }
-            return null;
         }
 
         private void OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
